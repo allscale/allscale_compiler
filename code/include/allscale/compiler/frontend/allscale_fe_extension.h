da0020b89dc750832d1306a1aaab2c1a93e0cfb7
Peter Thoman <petert@dps.uibk.ac.at>
2016-11-16 11:33:15 +0100

    Update insieme module

---
+++ code/include/allscale/compiler/frontend/allscale_fe_extension.h
@@ -0,0 +1,14 @@
+
+#include "insieme/frontend/extensions/frontend_extension.h"
+
+namespace allscale {
+namespace compiler {
+namespace frontend {
+
+	class AllscaleExtension : public insieme::frontend::extensions::FrontendExtension {
+
+	};
+
+}
+}
+}
