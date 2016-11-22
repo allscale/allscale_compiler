b7752dd9350ad696445f18718b68fddda9e7f032
Peter Zangerl <peterz@dps.uibk.ac.at>
2016-11-22 17:09:56 +0100

    frontend: Improve on prec translation.

--- code/src/frontend/allscale_fe_extension.cpp
+++ code/src/frontend/allscale_fe_extension.cpp
@@ -1,9 +1,13 @@
 
 #include "allscale/compiler/frontend/allscale_fe_extension.h"
 
+#include <boost/algorithm/string.hpp>
+
 #include "insieme/frontend/clang.h"
 #include "insieme/frontend/converter.h"
 #include "insieme/core/analysis/ir_utils.h"
+#include "insieme/core/transform/node_replacer.h"
+#include "insieme/utils/name_mangling.h"
 
 #include "allscale/compiler/lang/allscale_ir.h"
 #include "allscale/compiler/utils.h"
@@ -12,14 +16,46 @@ namespace allscale {
 namespace compiler {
 namespace frontend {
 
+	void AllscaleExtension::TranslationStateManager::pushState(const TranslationState translationState) {
+		translationStates.push_back(translationState);
+	}
+
+	AllscaleExtension::TranslationState AllscaleExtension::TranslationStateManager::getState() {
+		assert_false(translationStates.empty()) << "No TranslationState to pop";
+		return translationStates.back();
+	}
+
+	void AllscaleExtension::TranslationStateManager::popState() {
+		assert_false(translationStates.empty()) << "No TranslationState to pop";
+		translationStates.pop_back();
+	}
+
 	namespace {
+
+		core::TagTypePtr removeDuplicateCallOperators(const core::TagTypePtr& tagType) {
+			return core::transform::transformBottomUpGen(tagType, [](const core::MemberFunctionsPtr& memFuns) {
+				core::MemberFunctionList newMemFuns;
+				bool alreadyPresent = false;
+				for(const auto& memFun : memFuns) {
+					if(memFun->getNameAsString() == insieme::utils::mangle("operator()")) {
+						if(alreadyPresent) {
+							continue;
+						}
+						alreadyPresent = true;
+					}
+					newMemFuns.push_back(memFun);
+				}
+				return core::IRBuilder(memFuns->getNodeManager()).memberFunctions(newMemFuns);
+			});
+		}
 
 		core::ExpressionPtr handlePrecCall(const clang::CallExpr* call, insieme::frontend::conversion::Converter& converter) {
 			assert_eq(call->getNumArgs(), 1) << "handlePrecCall expects 1 argument only";
 			return lang::buildPrec(toVector(converter.convertCxxArgExpr(call->getArg(0))));
 		}
 
-		core::ExpressionPtr handleCoreFunCall(const clang::CallExpr* call, insieme::frontend::conversion::Converter& converter) {
+		core::ExpressionPtr handleCoreFunCall(const clang::CallExpr* call, insieme::frontend::conversion::Converter& converter,
+		                                      AllscaleExtension::TranslationStateManager& translationStateManager) {
 
 			assert_eq(call->getNumArgs(), 3) << "handleCoreFunCall expects 3 arguments";
 
@@ -62,6 +98,8 @@ namespace frontend {
 				baseBind = lang::buildLambdaToClosure(baseIr, baseClosureType);
 			}
 
+			translationStateManager.pushState({paramType, returnType});
+
 			// Handle step case
 
 			core::ExpressionPtr stepBind = nullptr;
@@ -77,13 +115,33 @@ namespace frontend {
 				auto stepClosureType = builder.functionType(toVector<core::TypePtr>(paramType, callableTupleType), stepReturnType, insieme::core::FK_CLOSURE);
 
 				stepBind = lang::buildLambdaToClosure(stepIr, stepClosureType);
+
+				auto genType = insieme::core::analysis::getReferencedType(stepIr->getType()).as<insieme::core::GenericTypePtr>();
+				auto tagType = tMap.at(genType);
+				auto newTagType = removeDuplicateCallOperators(tagType);
+				converter.getIRTranslationUnit().replaceType(genType, newTagType);
 			}
+
+			translationStateManager.popState();
 
 			return lang::buildBuildRecFun(cutoffBind, toVector(baseBind), toVector(stepBind));
 		}
 
 		core::ExpressionPtr handleCoreDoneCall(const clang::CallExpr* call, insieme::frontend::conversion::Converter& converter) {
 			return lang::buildTreetureDone(converter.convertCxxArgExpr(call->getArg(0)));
+		}
+
+		clang::CallExpr* extractCallExpr(clang::Expr* node) {
+			if(auto call = llvm::dyn_cast<clang::CallExpr>(node)) {
+				return call;
+
+			} else if(auto materialize = llvm::dyn_cast<clang::MaterializeTemporaryExpr>(node)) {
+				return extractCallExpr(materialize->GetTemporaryExpr());
+
+			} else if(auto construct = llvm::dyn_cast<clang::CXXConstructExpr>(node)) {
+				return extractCallExpr(construct->getArg(0));
+			}
+			return nullptr;
 		}
 	}
 
@@ -97,7 +155,7 @@ namespace frontend {
 					return handlePrecCall(call, converter);
 				}
 				if(name == "allscale::api::core::fun") {
-					return handleCoreFunCall(call, converter);
+					return handleCoreFunCall(call, converter, getTranslationStateManager());
 				}
 				if(name == "allscale::api::core::done") {
 					return handleCoreDoneCall(call, converter);
@@ -108,9 +166,39 @@ namespace frontend {
 		return nullptr;
 	}
 
+	insieme::frontend::stmtutils::StmtWrapper AllscaleExtension::Visit(const clang::Stmt* stmt, insieme::frontend::conversion::Converter& converter) {
+		// for variable declarations which are inited with a call to prec, store the type of the prec call in our mapping to replace the variable type
+		if(auto declStmt = llvm::dyn_cast<clang::DeclStmt>(stmt)) {
+			for(auto decl : declStmt->decls()) {
+				if(auto varDecl = llvm::dyn_cast<clang::VarDecl>(decl)) {
+					if(auto init = varDecl->getInit()) {
+						if(auto call = extractCallExpr(init)) {
+							auto decl = call->getCalleeDecl();
+							if(auto funDecl = llvm::dyn_cast_or_null<clang::FunctionDecl>(decl)) {
+								auto name = funDecl->getQualifiedNameAsString();
+								if(name == "allscale::api::core::prec") {
+									auto precCall = handlePrecCall(call, converter);
+									typeMappings[varDecl->getType()->getUnqualifiedDesugaredType()] = precCall->getType();
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return {};
+	}
+
+	insieme::core::TypePtr AllscaleExtension::Visit(const clang::QualType& type, insieme::frontend::conversion::Converter& converter) {
+		if(typeMappings.find(type->getUnqualifiedDesugaredType()) != typeMappings.end()) {
+			return typeMappings.at(type->getUnqualifiedDesugaredType());
+		}
+		return {};
+	}
+
 	insieme::core::TypePtr AllscaleExtension::PostVisit(const clang::QualType& typeIn, const insieme::core::TypePtr& irType,
 	                                                    insieme::frontend::conversion::Converter& converter) {
-		const clang::Type* type = typeIn.getTypePtr();
+		const clang::Type* type = typeIn->getUnqualifiedDesugaredType();
 		if(auto autoType = llvm::dyn_cast<clang::AutoType>(type)) {
 			type = autoType->getDeducedType().getTypePtr();
 		}
@@ -119,11 +207,19 @@ namespace frontend {
 			if(typeName == "allscale::api::core::detail::completed_task") {
 				return (core::GenericTypePtr) lang::TreetureType(core::analysis::getReferencedType(irType.as<core::GenericTypePtr>()->getTypeParameter(0)), false);
 			}
+			if(boost::starts_with(typeName, "allscale::api::core::detail::callable")) {
+				auto translationState = getTranslationStateManager().getState();
+				return (core::GenericTypePtr) lang::RecFunType(translationState.first, translationState.second);
+			}
 		}
 
 		return irType;
 	}
 
+	insieme::core::ProgramPtr AllscaleExtension::IRVisit(insieme::core::ProgramPtr& prog) {
+		dumpReadable(prog);
+		return prog;
+	}
 }
 }
 }
--- code/test/frontend/inputs/allscale_basic.cpp
+++ code/test/frontend/inputs/allscale_basic.cpp
@@ -12,6 +12,60 @@ int main() {
 		auto a = done(1);
 	}
 
+	#pragma test expect_ir(R"(
+		decl struct __any_string__cutoff;
+		decl struct __any_string__base;
+		decl struct __any_string__step;
+		decl IMP__operator_call_:const __any_string__cutoff::(int<4>) -> bool;
+		decl IMP__operator_call_:const __any_string__base::(int<4>) -> int<4>;
+		decl IMP__operator_call_:const __any_string__step::(int<4>, ref<recfun<int<4>,int<4>>,t,f,cpp_ref>) -> treeture<int<4>,f>;
+		decl IMP__conversion_operator_bool_space__lparen__star__rparen__lparen_int_rparen_:const __any_string__cutoff::() -> ptr<(int<4>) -> bool,t,f>;
+		decl IMP__conversion_operator_int_space__lparen__star__rparen__lparen_int_rparen_:const __any_string__base::() -> ptr<(int<4>) -> int<4>,t,f>;
+		def struct __any_string__step {
+			const function IMP__operator_call_ = (v133 : ref<int<4>,f,f,plain>, v134 : ref<recfun<int<4>,int<4>>,t,f,cpp_ref>) -> treeture<int<4>,f> {
+				return treeture_done(1);
+			}
+		};
+		def struct __any_string__base {
+			const function IMP__operator_call_ = (v81 : ref<int<4>,f,f,plain>) -> int<4> {
+				return *v81;
+			}
+		};
+		def struct __any_string__cutoff {
+			const function IMP__operator_call_ = (v35 : ref<int<4>,f,f,plain>) -> bool {
+				return *v35<2;
+			}
+		};
+		{
+			var ref<(int<4>) => treeture<int<4>,f>,f,f,plain> v147 = prec(
+					(build_recfun(
+							lambda_to_closure(
+									<ref<__any_string__cutoff,f,f,plain>>(ref_temp(type_lit(__any_string__cutoff))) {},
+									type_lit((int<4>) => bool)
+							),
+							[lambda_to_closure(
+									<ref<__any_string__base,f,f,plain>>(ref_temp(type_lit(__any_string__base))) {},
+									type_lit((int<4>) => int<4>)
+							)],
+							[lambda_to_closure(
+									<ref<__any_string__step,f,f,plain>>(ref_temp(type_lit(__any_string__step))) {},
+									type_lit((int<4>, (recfun<int<4>,int<4>>)) => treeture<int<4>,f>)
+							)]
+					))
+			);
+		}
+	)")
+	{
+		auto fib = prec(fun(
+				[](int x)->bool { return x < 2; },
+				[](int x)->int { return x; },
+				[](int x, const auto& f) {
+					return done(1);
+				}
+			)
+		);
+	}
+
 //	#pragma test expect_ir(R"({ var ref<int<4>> i = 42; })")
 //	{
 //		auto fib = prec(fun(
