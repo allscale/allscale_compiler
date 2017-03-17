4416e6365d5555b9d4eea9e9da78f7d1a9dc13f6
Peter Zangerl <peterz@dps.uibk.ac.at>
2017-03-17 10:07:01 +0100

    tests: Add second function to fibEager test - commented for now.

--- test/fib_eager/fib_eager.cpp
+++ test/fib_eager/fib_eager.cpp
@@ -22,15 +22,32 @@ int fibEager(int x) {
 	return f(x).get();
 }
 
+//int fibEagerReferences(int x) {
+//	auto f = prec(fun(
+//		[](const int& x) { return x < 2; },
+//		[](const int& x) { return x; },
+//		[](const int& x, const auto& rec) {
+//			auto a = run(rec(x-1));
+//			auto b = run(rec(x-2));
+//			return done(a.get() + b.get());
+//		}
+//	));
+//	return f(x).get();
+//}
+
 int main() {
 	const int N = 42;
 	const int fibN = fib_static(N);
 	const int fibE = fibEager(N);
+//	const int fibEr = fibEagerReferences(N);
 
+	std::cout << fibN << std::endl;
 	std::cout << fibE << std::endl;
+//	std::cout << fibEr << std::endl;
 
 	// run the evaluation
-	bool success = fibN == fibE;
+	bool success = fibN == fibE/* == fibEr*/;
+	std::cout << success << std::endl;
 
 	// return success state
 	return (success) ? 0 : 1;
