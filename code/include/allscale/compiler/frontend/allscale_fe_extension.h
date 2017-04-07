5c097df9219d31ef32a44d3052f574880a67b6ef
Peter Zangerl <peterz@dps.uibk.ac.at>
2017-04-07 13:28:34 +0200

    FE: Use back-ported Insieme type mapping infrastructure.

--- code/include/allscale/compiler/frontend/allscale_fe_extension.h
+++ code/include/allscale/compiler/frontend/allscale_fe_extension.h
@@ -1,25 +1,20 @@
 
 #pragma once
 
-#include "insieme/frontend/extensions/frontend_extension.h"
+#include "insieme/frontend/extensions/mapping_frontend_extension.h"
 
 #include <vector>
 #include <map>
 
 #include <boost/optional.hpp>
 
-#include "allscale/compiler/frontend/allscale_fe_extension_types.h"
 #include "allscale/compiler/frontend/allscale_fe_extension_exprs.h"
 
 namespace allscale {
 namespace compiler {
 namespace frontend {
 
-	class AllscaleExtension : public insieme::frontend::extensions::FrontendExtension {
-
-		detail::TypeMapper typeMapper;
-
-		detail::TypeMapper& getTypeMapper(insieme::frontend::conversion::Converter& converter);
+	class AllscaleExtension : public insieme::frontend::extensions::MappingFrontendExtension {
 
 	  protected:
 		virtual boost::optional<std::string> isPrerequisiteMissing(insieme::frontend::ConversionSetup& setup) const override;
@@ -27,6 +22,8 @@ namespace frontend {
 		// Types
 
 		virtual insieme::core::TypePtr Visit(const clang::QualType& type, insieme::frontend::conversion::Converter& converter) override;
+
+		virtual std::map<std::string, std::string> getTypeMappings() override;
 
 		// Expressions
 
