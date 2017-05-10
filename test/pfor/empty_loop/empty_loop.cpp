88ba16aaa0cbd2e0722ef5dc979dd0efcb2b8ab8
Herbert Jordan <herbert@dps.uibk.ac.at>
2017-05-10 14:26:21 +0200

    Added an empty loop pfor test to the integration tests.

---
+++ test/pfor/empty_loop/empty_loop.cpp
@@ -0,0 +1,13 @@
+#include <allscale/api/user/operator/pfor.h>
+
+using namespace allscale::api::user;
+
+int main() {
+
+    // just a simple, empty loop
+    pfor(0,10,[](int x){
+
+    });
+
+	return 0;
+}
