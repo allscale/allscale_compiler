8b4353fffb3212c2dab4830592d6ea97cfd964a4
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-07-17 13:08:45 +0200

    CBA: Add diagnostics analysis

---
+++ code/compiler/include/allscale/compiler/analysis/diagnostics.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <ostream>
+#include <vector>
+#include <string>
+
+#include "insieme/core/ir_address.h"
+
+#include "insieme/analysis/cba/haskell/context.h"
+
+namespace allscale {
+namespace compiler {
+namespace analysis {
+
+	/**
+	 * Severity of the Diagnostics Message
+	 */
+	enum class Severity : int {
+		Warning = 0,
+		Error = 1,
+	};
+
+	std::ostream& operator<<(std::ostream& out, Severity severity) {
+		switch(severity) {
+		case Severity::Warning: return out << "WARNING";
+		case Severity::Error:   return out << "ERROR";
+		}
+		return out;
+	}
+
+	class Issue {
+
+	  private:
+
+		  insieme::core::NodeAddress target;
+		  Severity severity;
+		  std::string message;
+
+	  public:
+
+		  Issue(insieme::core::NodeAddress target, Severity severity, std::string message)
+			  : target(target), severity(severity), message(message) {
+			  assert_true(target);
+		  }
+
+		  insieme::core::NodeAddress getTarget() const {
+			  return target;
+		  }
+
+		  Severity getSeverity() const {
+			  return severity;
+		  }
+
+		  std::string getMessage() const {
+			  return message;
+		  }
+
+	};
+
+	using Issues = std::vector<Issue>;
+
+	// a context object for re-using partial results of analysis calls
+	using AnalysisContext = insieme::analysis::cba::haskell::Context;
+
+	/**
+	 * The main entry point of the diagnostics analysis.
+	 *
+	 * @param context a context for the analysis to reuse partial results of previous analysis steps.
+	 * @param node the node to be analyzed
+	 * @return the list of diagnostic issues obtained for the given node
+	 */
+	Issues runDiagnostics(AnalysisContext& context, const insieme::core::NodeAddress& node);
+
+	/**
+	 * A convenience entry for the diagnostics analysis, producing a temporary analysis context.
+	 *
+	 * @param node the node to be analyzed
+	 * @return the list of diagnostic issues obtained for the given node
+	 */
+	Issues runDiagnostics(const insieme::core::NodeAddress& node);
+
+} // end namespace analysis
+} // end namespace compiler
+} // end namespace allscale
---
+++ code/compiler/src/analysis/allscale-hat/src/Allscale/Analysis/Diagnostics.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Allscale.Analysis.Diagnostics where
+
+import Control.Monad
+import Control.DeepSeq (NFData)
+import Data.Typeable (Typeable)
+import Foreign
+import Foreign.C.String
+import Foreign.C.Types
+import GHC.Generics (Generic)
+import Insieme.Adapter (CRep,CSet,CRepPtr,CSetPtr,CRepArr,updateContext,passNodeAddress,withArrayUnsignedLen)
+import Insieme.Inspire.NodeAddress
+
+import qualified Insieme.Analysis.Solver as Solver
+import qualified Insieme.Context as Ctx
+
+-- * Issues
+
+data Severity = Warning | Error
+  deriving (Eq, Ord, Show, Generic, NFData)
+
+data Issue = Issue { target   :: NodeAddress
+                   , severity :: Severity
+                   , message  :: String
+                   }
+  deriving (Eq, Ord, Show, Generic, NFData)
+
+newtype Issues = Issues [Issue]
+  deriving (Eq, Ord, Show, Generic, NFData)
+
+instance Solver.Lattice Issues where
+    bot = Issues []
+    Issues x `merge` Issues y = Issues $ x ++ y
+
+-- * Analysis
+
+data DataRequirementAnalysis = DataRequirementAnalysis
+  deriving (Typeable)
+
+diagnosticsAnalysis :: NodeAddress -> Solver.TypedVar Issues
+diagnosticsAnalysis addr = Solver.mkVariable (idGen addr) []
+                         $ Issues [ Issue addr Warning "Diagnostics output not implemented."
+                                  , Issue addr Error "You Must Construct Additional Pylons!"
+                                  ]
+  where
+    analysis = Solver.mkAnalysisIdentifier DataRequirementAnalysis "DIAG"
+
+    idGen = Solver.mkIdentifierFromExpression analysis
+
+-- * FFI
+
+foreign export ccall "hat_hs_diagnostics"
+  hsDiagnostics :: StablePtr Ctx.Context -> StablePtr NodeAddress -> IO (CRepPtr Issues)
+
+hsDiagnostics ctx_hs expr_hs = do
+    ctx  <- deRefStablePtr ctx_hs
+    expr <- deRefStablePtr expr_hs
+    let (res,ns) = Solver.resolve (Ctx.getSolverState ctx) (diagnosticsAnalysis expr)
+    let (ctx_c) = Ctx.getCContext ctx
+    ctx_nhs <- newStablePtr $ ctx { Ctx.getSolverState = ns }
+    updateContext ctx_c ctx_nhs
+    passIssues ctx_c res
+
+foreign import ccall "hat_c_mk_issue"
+  mkCIssue :: CRepPtr NodeAddress -> CInt -> CString -> IO (CRepPtr Issue)
+
+foreign import ccall "hat_c_mk_issues"
+  mkCIssues :: CRepArr Issue -> CSize -> IO (CRepPtr Issues)
+
+passIssues :: Ctx.CContext -> Issues -> IO (CRepPtr Issues)
+passIssues ctx (Issues is) = do
+    is_c <- forM is passIssue
+    withArrayUnsignedLen is_c mkCIssues
+  where
+    passIssue :: Issue -> IO (CRepPtr Issue)
+    passIssue (Issue t s m) = do
+        t_c <- passNodeAddress ctx t
+        withCString m $ mkCIssue t_c (convertSeverity s)
+
+    convertSeverity :: Severity -> CInt
+    convertSeverity Warning = 0
+    convertSeverity Error = 1
