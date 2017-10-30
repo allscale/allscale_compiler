4ab6ee23dff7ea05672935b6689fe87ff20caa46
Herbert Jordan <herbert@dps.uibk.ac.at>
2017-10-30 15:45:54 +0100

    Extended grid_init_2d test case with additional checks.

--- test/data_requirements/grid_init_2d/grid_init_2d.cpp
+++ test/data_requirements/grid_init_2d/grid_init_2d.cpp
@@ -4,6 +4,10 @@
 using namespace allscale::api::user::algorithm;
 using namespace allscale::api::user::data;
 
+void expect_eq(int a, int b) {
+    if (a != b) exit(1);
+}
+
 int main() {
 
     const int N = 1000;
@@ -18,9 +22,35 @@ int main() {
         }
     });
 
+    // check the initialization
+    pfor(Point{0,0},Point{N,N},[&](const Point& p) {
+        expect_eq(1,data[p]);
+    });
+
+    // ---
+
+    // initialize grid - column-wise
+    pfor(0,N,[&](int i){
+        for(int j=0; j<N; j++) {
+            data[{j,i}] = 2;
+        }
+    });
+
+    // check the initialization
+    pfor(Point{0,0},Point{N,N},[&](const Point& p) {
+        expect_eq(2,data[p]);
+    });
+
+    // ---
+
     // initialize grid - collapsed
     pfor(Point{0,0},Point{N,N},[&](const Point& p) {
-        data[p] = 2;
+        data[p] = 3;
+    });
+
+    // check the initialization
+    pfor(Point{0,0},Point{N,N},[&](const Point& p) {
+        expect_eq(3,data[p]);
     });
 
 	return 0;
