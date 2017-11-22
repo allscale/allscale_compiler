a75d7ddf5bc21228fde0f88889e0e1f685114675
Philipp Gschwandtner <philipp@dps.uibk.ac.at>
2017-11-22 16:26:55 +0100

    annotations: fixed data item tag annotation not dumpable

--- code/compiler/include/allscale/compiler/core/data_item_annotation.h
+++ code/compiler/include/allscale/compiler/core/data_item_annotation.h
@@ -2,14 +2,11 @@
 
 #include "allscale/compiler/core/allscale_core.h"
 
-#include "insieme/core/dump/annotations.h"
 #include "insieme/core/ir_node.h"
 
 namespace allscale {
 namespace compiler {
 namespace core {
-
-	struct DataItemTag : public insieme::core::value_annotation::copy_on_migration {};
 
 	void markAsDataItem(const insieme::core::TypePtr& node);
 	bool isDataItem(const insieme::core::NodePtr& node);
--- code/compiler/src/core/data_item_annotation.cpp
+++ code/compiler/src/core/data_item_annotation.cpp
@@ -2,34 +2,39 @@
 #include "allscale/compiler/core/data_item_annotation.h"
 
 #include "insieme/core/lang/basic.h"
+#include "insieme/core/ir_node_annotation.h"
+#include "insieme/core/dump/annotations.h"
+#include "insieme/core/encoder/encoder.h"
 
 
 namespace allscale {
 namespace compiler {
 namespace core {
 
+	struct DataItemTag : public insieme::core::value_annotation::copy_on_migration {};
+
 namespace {
 
-	/**
-	* A Converter ensuring that data item tags are preserved within binary dumps.
-	*/
-	struct DataItemTagConverter : public insieme::core::dump::AnnotationConverter {
+	using namespace insieme::core;
 
-		DataItemTagConverter() : AnnotationConverter("DataItemTag") {}
+	// ---------------- Support Dump ----------------------
+
+	VALUE_ANNOTATION_CONVERTER(DataItemTag)
+
+		typedef core::value_node_annotation<DataItemTag>::type annotation_type;
 
 		virtual insieme::core::ExpressionPtr toIR(insieme::core::NodeManager& manager, const insieme::core::NodeAnnotationPtr& annotation) const override {
-			// the actual node here is not really important ... it just must not be null
-			return manager.getLangBasic().getTrue();
+			assert_true(dynamic_pointer_cast<annotation_type>(annotation)) << "Only data item tag annotations supported!";
+			return encoder::toIR(manager, string("dataitem"));
 		}
 
 		virtual insieme::core::NodeAnnotationPtr toAnnotation(const insieme::core::ExpressionPtr& node) const override {
+			assert_true(encoder::isEncodingOf<string>(node.as<ExpressionPtr>())) << "Invalid encoding encountered!";
 			return std::make_shared<typename insieme::core::value_node_annotation<DataItemTag>::type>(DataItemTag());
 		}
 
-	};
+	VALUE_ANNOTATION_CONVERTER_END
 
-	// register the converter into the central annotation converter registry
-	__insieme_unused bool reg = insieme::core::dump::AnnotationConverterRegister::getDefault().registerConverter<DataItemTagConverter, DataItemTag>();
 }
 
 void markAsDataItem(const insieme::core::TypePtr& node) {
