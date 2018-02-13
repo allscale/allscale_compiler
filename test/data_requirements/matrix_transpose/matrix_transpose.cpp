6785ec277edc8242aabf18f93383e801fa86ea4c
Herbert Jordan <herbert@dps.uibk.ac.at>
2018-02-13 14:05:46 +0100

    Added matrix transpose test case.

---
+++ test/data_requirements/matrix_transpose/matrix_transpose.cpp
@@ -0,0 +1,39 @@
+
+#include <chrono>
+
+#include <allscale/api/user/algorithm/pfor.h>
+#include <allscale/api/user/data/static_grid.h>
+
+const int N = 1000;
+
+using namespace allscale::api::user::data;
+using namespace allscale::api::user::algorithm;
+
+using Element = double;
+using Matrix = StaticGrid<Element,N,N>;
+using Point = typename Matrix::coordinate_type;
+
+using myClock = std::chrono::high_resolution_clock;
+
+int main() {
+
+	// create two matrixes
+	Matrix A;
+	Matrix B;
+
+	auto start = myClock::now();
+
+	// perform transformation
+	pfor(Point{N,N},[&](const Point& p) {
+		B[{p.y,p.x}] = A[p];
+		A[p]++;
+	});
+
+	auto end = myClock::now();
+
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cerr << "Completed in " << ms << "ms\n";
+	std::cerr << "Throughput: " << ((sizeof(Element)*N*N)/(double)ms/(1024*1024)) << "MiB/s\n";
+
+	return EXIT_SUCCESS;
+}
