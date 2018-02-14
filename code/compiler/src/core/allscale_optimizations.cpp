9808f3b13cd95b3641e708aa42323afcae51ea89
Peter Zangerl <peterz@dps.uibk.ac.at>
2018-02-14 11:18:29 +0100

    core: Improved get_data_item hoisting application.

--- code/compiler/src/core/allscale_optimizations.cpp
+++ code/compiler/src/core/allscale_optimizations.cpp
@@ -51,35 +51,34 @@ namespace core {
 			const auto& body = lambdaExpr->getBody();
 
 			// collect all occurrences of art_data_item_get in this lambda
-			core::ExpressionAddressList getCallAddresses;
-			core::visitDepthFirstOncePrunable(core::CompoundStmtAddress(body), [&](const core::ExpressionAddress& expr){
+			core::ExpressionList getCallNodes;
+			core::visitDepthFirstOncePrunable(body, [&](const core::ExpressionPtr& expr) {
 				// don't descend into other lambda expressions
-				if(expr.isa<core::LambdaExprAddress>()) return core::Action::Prune;
+				if(expr.isa<core::LambdaExprPtr>()) return core::Action::Prune;
 
 				// and collect addresses of calls to getDataItem
-				const auto& getCall = expr.getAddressedNode();
-				if(beExt.isCallOfGetDataItem(getCall)) {
+				if(beExt.isCallOfGetDataItem(expr)) {
 					// if we are only using parameters of the current lambda, we are allowed to hoist this call
-					if(usesOnlyLambdaParameters(getCall, lambdaExpr)) {
-						getCallAddresses.push_back(expr);
+					if(usesOnlyLambdaParameters(expr, lambdaExpr)) {
+						getCallNodes.push_back(expr);
 					}
 				}
 				return core::Action::Descent;
 			}, false);
 
 			// if we found calls
-			if(!getCallAddresses.empty()) {
+			if(!getCallNodes.empty()) {
 				// we create replacement variables
 				core::DeclarationStmtList declStmts;
-				std::map<core::NodeAddress, core::NodePtr> replacements;
+				core::NodeMap replacements;
 				// for each call
-				for(const auto& addr : getCallAddresses) {
+				for(const auto& callNode : getCallNodes) {
 					// as the types of dataItemGet differ between IR and C++ we need to wrap them here (with unpack and pack) to get correctly typed IR
-					auto declStmt = builder.declarationStmt(builder.callExpr(beExt.getGetDataItemUnpack(), addr.getAddressedNode()));
+					auto declStmt = builder.declarationStmt(builder.callExpr(beExt.getGetDataItemUnpack(), callNode));
 					declStmts.push_back(declStmt);
-					replacements.insert({addr, builder.callExpr(beExt.getGetDataItemPack(), declStmt->getVariable())});
+					replacements.insert({callNode, builder.callExpr(beExt.getGetDataItemPack(), declStmt->getVariable())});
 				}
-				auto newBody = core::transform::replaceAll(mgr, replacements).as<core::CompoundStmtPtr>();
+				auto newBody = core::transform::replaceAll(mgr, body, replacements, core::transform::localReplacement).as<core::CompoundStmtPtr>();
 
 				// and prepend them to old body
 				core::StatementList bodyStmts = newBody->getStatements();
